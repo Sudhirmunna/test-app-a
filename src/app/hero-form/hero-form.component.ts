806798b5b51689de3569b843ef73c1de811ce816
Shashikanth Reddy <shashi4489@gmail.com>
2020-11-18 03:57:37 -0600

    reactive forms

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Hero } from '../hero';
 
 @Component({
@@ -8,20 +9,40 @@ import { Hero } from '../hero';
 })
 export class HeroFormComponent implements OnInit {
   list = [];
+  form: FormGroup;
 
-  model = new Hero(18, 3, 10);
+
 
   submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
 
 
   onSubmit() {
+    var obj = Object.assign({}, this.form.value);
+   
+    Object.keys(obj).forEach(key => obj[key] == undefined || obj[key] == '' ? delete obj[key] : '');
     this.submitted = true;
+    // stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+    this.printList(this.form.controls.green.value);
+  }
+
+  get f() { return this.form.controls; }
+
+  createForm() {
+    this.form = this.formBuilder.group({
+      blue: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
+      yellow: ['', [Validators.required, Validators.min(1), Validators.max(10)]],
+      green: ['', [Validators.required, Validators.min(1)]]
+    })
   }
 
 
@@ -30,7 +51,6 @@ export class HeroFormComponent implements OnInit {
     for (let i = 1; i < maxValue; i++) {
       this.list.push(i);
     }
-    this.submitted = false;
     return this.list;
   }
 }
